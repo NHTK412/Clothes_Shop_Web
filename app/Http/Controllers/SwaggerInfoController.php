dbc2bf2cf1a90944acce161d9a9cdd118163f291
dinhfu05 <dinhphu511205@gmail.com>
2026-06-14 22:26:44 +0700

    feature(admin/products): Add CRUD API for product management tasks. #8

---
+++ app/Http/Controllers/SwaggerInfoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Clothes Shop API",
+ *     description="API documentation for Clothes Shop Web",
+ * )
+ */
+class SwaggerInfoController extends Controller
+{
+    // This class only holds OpenAPI annotations for l5-swagger.
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,19 @@
 <?php
 
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Clothes Shop API",
+ *     description="API documentation for Clothes Shop Web",
+ * )
+ */
+
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
+// Admin product management
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update');
+Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
